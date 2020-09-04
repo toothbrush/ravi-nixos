28e29f230a2eb2314903711a095a510dfa1c545b
root <root@ravi>
2020-09-04 20:58:21 +1000

    Make GnuPG / pinentry work.

--- configuration.nix
+++ configuration.nix
@@ -89,11 +89,12 @@
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  #   pinentryFlavor = "gnome3";
-  # };
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+    pinentryFlavor = "gtk2";
+    defaultCacheTTL = 3600;
+  };
 
   # List services that you want to enable:
 
