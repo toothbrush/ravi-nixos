b87c58650f0c364034156e53f37c4eb4261a0940
root <root@ravi>
2020-09-06 16:49:47 +1000

    use 20.09-style settings for tlp

--- configuration.nix
+++ configuration.nix
@@ -130,18 +130,18 @@ in
   # services.xserver.xkbOptions = "eurosign:e";
   services.tlp = {
     enable = true;
-    extraConfig = ''
-      START_CHARGE_THRESH_BAT0=65
-      STOP_CHARGE_THRESH_BAT0=80
-      DEVICES_TO_DISABLE_ON_STARTUP="bluetooth wwan"
-      CPU_BOOST_ON_AC=1
-      CPU_BOOST_ON_BAT=0
-      CPU_ENERGY_PERF_POLICY_ON_AC=performance
-      CPU_ENERGY_PERF_POLICY_ON_BAT=balance_power
-      CPU_SCALING_GOVERNOR_ON_AC=performance
-      CPU_SCALING_GOVERNOR_ON_BAT=powersave
-      RUNTIME_PM_DRIVER_BLACKLIST="mei_me"
-    '';
+    settings = {
+      START_CHARGE_THRESH_BAT0 = 65;
+      STOP_CHARGE_THRESH_BAT0 = 80;
+      DEVICES_TO_DISABLE_ON_STARTUP = "bluetooth wwan";
+      CPU_BOOST_ON_AC = 1;
+      CPU_BOOST_ON_BAT = 0;
+      CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+      CPU_ENERGY_PERF_POLICY_ON_BAT = "balance_power";
+      CPU_SCALING_GOVERNOR_ON_AC = "performance";
+      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+      RUNTIME_PM_DRIVER_BLACKLIST = "mei_me";
+    };
   };
 
   services.xserver.xkbOptions = "ctrl:nocaps";
