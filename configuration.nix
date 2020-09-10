2c28789cedcc6b92386d9ede475fd33fa06c9eef
root <root@ravi>
2020-09-10 15:39:19 +1000

    Add ~/bin to $PATH for graphical sessions.

--- configuration.nix
+++ configuration.nix
@@ -230,6 +230,7 @@ in
     ${pkgs.xorg.xkbcomp}/bin/xkbcomp ${compiledDefaultLayout} $DISPLAY
     export THINKPAD_KBD_ID=$(xinput list --id-only 'AT Translated Set 2 keyboard')
     ${pkgs.xorg.xkbcomp}/bin/xkbcomp -i $THINKPAD_KBD_ID ${compiledInternalLayout} $DISPLAY
+    export PATH="$HOME/bin:$PATH"
   '';
 
   # Enable touchpad support.
