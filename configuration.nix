933a4e08cd388a6258a106efa03d753e832f37ba
root <root@ravi>
2020-09-10 12:42:15 +1000

    Remove proxy settings.

--- configuration.nix
+++ configuration.nix
@@ -71,10 +71,6 @@ in
   networking.networkmanager.enable = true;
   networking.nameservers = [ "1.1.1.1" "8.8.8.8" "8.8.4.4" ];
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
   # Select internationalisation properties.
   i18n.defaultLocale = "en_AU.UTF-8";
 
