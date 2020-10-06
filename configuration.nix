18bc49cd60228d34b309c765c86219b40a56ff79
root <root@ravi>
2020-10-06 15:36:20 +0930

    Move installed packages list into a separate .nix file.

--- configuration.nix
+++ configuration.nix
@@ -15,6 +15,8 @@ in
   imports = [
     # Include the results of the hardware scan.
     ./hardware-configuration.nix
+    # Split out file with long list of system-wide packages.
+    ./installed-packages.nix
   ];
 
   nixpkgs.config.allowUnfree = true;
@@ -73,88 +75,6 @@ in
 
   # Set your time zone.
   time.timeZone = "Australia/Darwin";
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages =
-    with pkgs; [
-      arandr
-      arc-theme
-      aspell
-      aspellDicts.en
-      aspellDicts.fr
-      aspellDicts.nl
-      autojump
-      aws-vault
-      awscli
-      bc
-      binutils # for `ar' command.
-      brightnessctl
-      cargo
-      coreutils
-      direnv
-      discount # for markdown
-      dmenu
-      dunst
-      dwm
-      emacs
-      firefox
-      flameshot
-      fzf
-      gcc
-      git
-      glxinfo
-      gnucash
-      gnumake
-      gnupg
-      go
-      gopls
-      htop
-      i3lock
-      irssi
-      isync
-      jq
-      mu
-      ncdu
-      nixpkgs-fmt
-      openvpn
-      paper-icon-theme
-      pass
-      pavucontrol
-      pciutils
-      perl
-      pinentry-gtk2
-      pkgconfig
-      powertop
-      restic
-      rsync
-      rust-analyzer
-      rustc
-      rustfmt
-      rxvt-unicode
-      shellcheck
-      signal-desktop
-      silver-searcher
-      spotify
-      st
-      stow
-      tree
-      unzip
-      vimHugeX
-      vlc
-      w3m
-      wget
-      xclip
-      xdotool
-      xmobar
-      xmonad-with-packages
-      xorg.xev
-      xorg.xkbcomp
-      xorg.xrandr
-      youtube-dl
-      zsh-history-substring-search
-      zsh-syntax-highlighting
-    ];
 
   environment.homeBinInPath = true;
 
---
+++ installed-packages.nix
@@ -0,0 +1,85 @@
+{ pkgs, ... }:
+
+{
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages =
+    with pkgs; [
+      arandr
+      arc-theme
+      aspell
+      aspellDicts.en
+      aspellDicts.fr
+      aspellDicts.nl
+      autojump
+      aws-vault
+      awscli
+      bc
+      binutils # for `ar' command.
+      brightnessctl
+      cargo
+      coreutils
+      direnv
+      discount # for markdown
+      dmenu
+      dunst
+      dwm
+      emacs
+      firefox
+      flameshot
+      fzf
+      gcc
+      git
+      glxinfo
+      gnucash
+      gnumake
+      gnupg
+      go
+      gopls
+      htop
+      i3lock
+      irssi
+      isync
+      jq
+      mu
+      ncdu
+      nixpkgs-fmt
+      openvpn
+      paper-icon-theme
+      pass
+      pavucontrol
+      pciutils
+      perl
+      pinentry-gtk2
+      pkgconfig
+      powertop
+      restic
+      rsync
+      rust-analyzer
+      rustc
+      rustfmt
+      rxvt-unicode
+      shellcheck
+      signal-desktop
+      silver-searcher
+      spotify
+      st
+      stow
+      tree
+      unzip
+      vimHugeX
+      vlc
+      w3m
+      wget
+      xclip
+      xdotool
+      xmobar
+      xmonad-with-packages
+      xorg.xev
+      xorg.xkbcomp
+      xorg.xrandr
+      youtube-dl
+      zsh-history-substring-search
+      zsh-syntax-highlighting
+    ];
+}
