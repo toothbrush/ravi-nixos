cc0de2b14c4a6162943999f975716058a5d72cba
root <root@ravi>
2021-02-21 10:54:22 +0930

    reformat hardware-configuration.nix, no changes.

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -5,7 +5,8 @@
 
 {
   imports =
-    [ (modulesPath + "/installer/scan/not-detected.nix")
+    [
+      (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
   boot.initrd.availableKernelModules = [ "xhci_pci" "nvme" "usb_storage" "sd_mod" "rtsx_pci_sdmmc" ];
@@ -14,18 +15,19 @@
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/afe92867-a848-4020-9660-5c995a6d51c7";
+    {
+      device = "/dev/disk/by-uuid/afe92867-a848-4020-9660-5c995a6d51c7";
       fsType = "btrfs";
     };
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/849A-B888";
+    {
+      device = "/dev/disk/by-uuid/849A-B888";
       fsType = "vfat";
     };
 
   swapDevices =
-    [ { device = "/dev/disk/by-uuid/d029460c-1463-4ad2-812d-2ebf9ab668f9"; }
-    ];
+    [{ device = "/dev/disk/by-uuid/d029460c-1463-4ad2-812d-2ebf9ab668f9"; }];
 
   powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
 }
